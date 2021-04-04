eeaa767386bcd576581fe5d3fedaf4a8b6278239
adisakshya <adisakshya98@gmail.com>
2021-04-04 12:33:50 +0530

    added: carbon assets

---
+++ assets/profile.js
@@ -0,0 +1,22 @@
+let adisakshya = {
+  name: 'Adisakshya Chauhan',
+  bio: 'Student Developer',
+  pronouns: 'he' | 'his',
+  locaion: '28.6245° N, 77.0914° E',
+  currentWorkplace: '👨🏻‍💼 Comming soon...',
+  digitalBusinessCard: '$ npx adisakshya',
+  knowledgeBase: [
+    '🐱‍💻 Software Engineering & Development',
+    '🐱‍👤 DevOps',
+    '🐱‍🏍 Cloud Computing',
+    '🤖 Machine Learning',
+  ],
+  askMeAbout: ['🌌 Science', '👨🏻‍💻 Technology', '⚽ Football'],
+  links: {
+    webSite: 'adisakshya.codes',
+    liveChat: 'chat.adisakshya.codes',
+    cv: 'cv.adisakshya.codes'
+  },
+  funFact: 'First of his name on Earth',
+  quote: 'There is always more to reality than one imagines - Prof. Paradox'
+}
